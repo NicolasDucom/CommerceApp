58b965f45ea99f6f1fb0c667ada5b61ca10d7b57
Nicolas <nducom@gmail.com>
2015-04-16 13:23:14 +0200

    first commit

---
+++ ECommerceApp/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ECommerceApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.libelle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prix = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Stock = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Couleur = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idpack = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.libelle,
+            this.Prix,
+            this.Stock,
+            this.Couleur,
+            this.idpack});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 56);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(636, 377);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.Visible = false;
+            // 
+            // libelle
+            // 
+            this.libelle.HeaderText = "Libellé";
+            this.libelle.Name = "libelle";
+            // 
+            // Prix
+            // 
+            this.Prix.HeaderText = "Prix";
+            this.Prix.Name = "Prix";
+            // 
+            // Stock
+            // 
+            this.Stock.HeaderText = "Stock";
+            this.Stock.Name = "Stock";
+            // 
+            // Couleur
+            // 
+            this.Couleur.HeaderText = "Couleur";
+            this.Couleur.Name = "Couleur";
+            // 
+            // idpack
+            // 
+            this.idpack.HeaderText = "idpack";
+            this.idpack.Name = "idpack";
+            this.idpack.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Acheter le pack correspondant au produit selectionné";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 450);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 476);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn libelle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prix;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Stock;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Couleur;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idpack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ECommerceApp/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ECommerceApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            xmlRead();
+        }
+
+        /// <summary>
+        /// Lis le fichier XML et rempli de datagridview
+        /// </summary>
+        private void xmlRead()
+        {
+            try
+            {
+                XDocument document = XDocument.Load("products.xml");
+                var products = from p in document.Descendants("product")
+                               select new
+                                   {
+                                       id = p.Attribute("id").Value,
+                                       libelle = p.Element("libelle").Value,
+                                       prix = p.Element("prix").Value,
+                                       stock = p.Element("stock").Value,
+                                       couleur = p.Element("couleur").Value,
+                                       idPack = p.Element("idpack").Value,
+                                   };
+                foreach (var p in products)
+                {
+                    dataGridView1.Rows.Add(new object[] { p.id, p.libelle, p.prix, p.stock, p.couleur, p.idPack });
+                }
+                dataGridView1.AutoResizeColumns();
+            }
+            catch (Exception)
+            {
+                label2.Text = "Erreur lors de la lecture du fichier !";
+            }
+        }
+
+        /// <summary>
+        /// Cherche les elements d'un pack dans le fichier XML et les rajoute a un pack dans le fichier paniers
+        /// </summary>
+        private void buyPackById(string id, string pack)
+        {
+            try
+            {
+                XDocument document = XDocument.Load("products.xml");
+                var products = from p in document.Descendants("product")
+                               where p.Element("idpack").Value == pack
+                               select new
+                               {
+                                   id = p.Attribute("id").Value,
+                                   libelle = p.Element("libelle").Value,
+                                   prix = p.Element("prix").Value,
+                                   stock = p.Element("stock").Value,
+                                   couleur = p.Element("couleur").Value,
+                                   idPack = p.Element("idpack").Value,
+                               };
+                if (!File.Exists("paniers.xml"))
+                {
+                    XElement XML_Nouveau = new XElement("Paniers");
+                    XML_Nouveau.Save("paniers.xml");
+                }
+                string nomDuPack = nomPack(pack);
+                string prixDuPack = prixPack(pack);
+
+                XDocument document2 = XDocument.Load("paniers.xml");
+                XElement newPack = new XElement("Pack",
+                    new XElement("Nom", nomDuPack),
+                    new XElement("Prix", prixDuPack),
+                    new XElement("Produits")
+                 );
+
+                foreach (var p in products)
+                {
+                    newPack.Element("Produits").Add(
+                        new XElement("product",
+                            new XAttribute("id", p.id),
+                            new XElement("libelle", p.libelle),
+                            new XElement("prix", p.prix),
+                            new XElement("stock", p.stock),
+                            new XElement("couleur", p.couleur),
+                            new XElement("idpack", p.idPack)
+                            )
+                        );
+                }
+                document2.Element("Paniers").Add(newPack);
+                try
+                {
+                    document2.Save("paniers.xml");
+                    label2.Text = nomDuPack + " ajouté !";
+                }
+                catch (Exception)
+                {
+                    label2.Text = "Erreur lors de l'ajout du pack!";
+                }
+            }
+            catch (Exception)
+            {
+                label2.Text = "Erreur lors de l'ouverture du fichier XML";
+                return;
+            }
+           
+        }
+
+        /// <summary>
+        /// Retourne le nom associé à un pack
+        /// </summary>
+        private string nomPack(string pack)
+        {
+            string nomDuPack = "";
+            switch (pack)
+            {
+                case "1":
+                    nomDuPack = "Pack cuivre";
+                    break;
+                case "2":
+                    nomDuPack = "Pack argent";
+                    break;
+                case "3":
+                    nomDuPack = "Pack or";
+                    break;
+                case "4":
+                    nomDuPack = "Pack platine";
+                    break;
+                default :
+                    nomDuPack =  "";
+                    break;
+            }
+            return nomDuPack;
+        }
+
+        /// <summary>
+        /// retourne le prix associé à un pack
+        /// </summary>
+        private string prixPack(string pack)
+        {
+            string prixDuPack = "";
+            switch (pack)
+            {
+                case "1":
+                    prixDuPack = "19 €";
+                    break;
+                case "2":
+                    prixDuPack = "29 €";
+                    break;
+                case "3":
+                    prixDuPack = "39 €";
+                    break;
+                case "4":
+                    prixDuPack = "99 €";
+                    break;
+                default:
+                    prixDuPack = "";
+                    break;
+            }
+            return prixDuPack;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                buyPackById(dataGridView1.SelectedRows[0].Cells[0].Value.ToString(), dataGridView1.SelectedRows[0].Cells[5].Value.ToString());
+            }
+            catch (Exception)
+            {
+                label2.Text = "Pas de pack selectionné !";
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            string idpack = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+            button1.Text = "Acheter le " + nomPack(idpack) + " à " + prixPack(idpack);
+        }
+    }
+}
